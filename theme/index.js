1701e0aee983986b689b6ab690f348eb7bd705cb
mdarora <mdarora0001@gmail.com>
2023-12-31 01:06:44 -0500

    theme colors

---
+++ theme/index.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native"
+
+export const theme = {
+    background: '#eab308',
+    text: '#eab308'
+}
+export const styles = StyleSheet.create({
+    text: { color: theme.text },
+    background: { backgroundColor: theme.background }
+});
