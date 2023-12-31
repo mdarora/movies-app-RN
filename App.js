4898877084d4e5964db0d82c863732bb97a29065
mdarora <mdarora0001@gmail.com>
2023-12-31 16:41:44 -0500

    created a person screen for casts

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import MovieScreen from './screens/MovieScreen';
+import PersonScreen from './screens/PersonScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="Movie" component={MovieScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Person" component={PersonScreen} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
