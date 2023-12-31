b6db79211480e11581a4a32dc58547361c4d928f
mdarora <mdarora0001@gmail.com>
2023-12-30 23:44:01 -0500

    seted up tailwind css with nativewind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StatusBar } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className='flex-1 items-center justify-center'>
+        <StatusBar />
+      <Text className='text-red-500 text-xl font-bold'>HomeScreen</Text>
     </View>
   )
 }
