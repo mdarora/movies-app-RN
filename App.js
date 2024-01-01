a2aa10ae6681768347739817e04520bebe3f5059
mdarora <mdarora0001@gmail.com>
2024-01-01 13:58:54 -0500

    created a screen for searching

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import MovieScreen from './screens/MovieScreen';
 import PersonScreen from './screens/PersonScreen';
+import SearchScreen from './screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="Movie" component={MovieScreen} options={{headerShown: false}} />
         <Stack.Screen name="Person" component={PersonScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Search" component={SearchScreen} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
