1d118276809fd55de965f9332220218e4f59aa77
mdarora <mdarora0001@gmail.com>
2024-01-10 22:10:20 -0500

    searching movies from api

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,31 +1,66 @@
-import { View, Text, useWindowDimensions, SafeAreaView, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback } from 'react-native'
+import { View, Text, useWindowDimensions, SafeAreaView, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { XMarkIcon } from 'react-native-heroicons/outline';
+import { ChevronLeftIcon, XMarkIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import { Image } from 'react-native';
+import { styles, theme } from '../theme';
+import { API_BASE_URL, API_IMAGE_URL, API_KEY } from '../constants';
 
 const ios = Platform.OS === 'ios';
 
 const SearchScreen = () => {
     const navigation = useNavigation();
     const { height, width } = useWindowDimensions();
+
+    const [searchInput, setSearchInput] = useState('');
+    const [result, setResult] = useState([]);
+
+    const handleSearch =async () => {
+        if (searchInput.trim().length > 0) {
+            try {
+                const res =await fetch(`${API_BASE_URL}/search/movie?query=${searchInput.trim()}&language=en-US&page=1&api_key=${API_KEY}`);
+                const data =await res.json();
+                data && data.results && setResult(data.results);
+            } catch (error) {
+                console.error('error searching movies : ', error);
+            }
+        } else {
+            Alert.alert('Input is empty');
+        }
+    }
+
+
     const movieName = 'Lorem ipsum dolor sit amet';
-    const [result, setResult] = useState([1,2,3,4,5]);
     return (
         <View className={`bg-neutral-900 flex-1`}>
             <SafeAreaView className={`flex-1 ${ios ? '' : 'mt-12'}`}>
                 <StatusBar style={'light'} />
 
-                <View className='mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full'>
-                    <TextInput placeholder='Search Movie' placeholderTextColor={'lightgray'}
-                        className='flex-1 px-4 py-2 text-base text-white font-semibold tracking-wide'
-                    />
-                    <TouchableOpacity className='bg-neutral-500 rounded-full p-3 m-1'
-                        onPress={() => navigation.goBack()} >
-                        <XMarkIcon size={25} color='white'></XMarkIcon>
+                <SafeAreaView className={`mx-4 mb-4 flex-row justify-between items-center ${ios ? '' : 'mt-12'}`}>
+                    <TouchableOpacity style={styles.background} className='rounded-xl p-1 mr-1' onPress={() => navigation.goBack()}>
+                        <ChevronLeftIcon size={25} strokeWidth={2.5} color={'white'} />
                     </TouchableOpacity>
-                </View>
+
+                    <View className='flex-row flex-1 justify-between items-center border border-neutral-500 rounded-full'>
+                        <View className='overflow-hidden'>
+                            <TextInput placeholder='Search Movie' placeholderTextColor={'lightgray'} value={searchInput} onChangeText={(value) => setSearchInput(value)}
+                                className='px-4 py-2 text-base text-white font-semibold tracking-wide rounded-full flex-1'
+                            />
+                        </View>
+                    </View>
+                    <View className='flex-row items-center'>
+                        <TouchableOpacity className=''
+                            onPress={() => setSearchInput('')} >
+                            <XMarkIcon size={25} color='gray' />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity className='bg-neutral-500 rounded-full p-3 m-1'
+                            onPress={handleSearch} >
+                            <MagnifyingGlassIcon size={25} color={theme.text} />
+                        </TouchableOpacity>
+                    </View>
+                </SafeAreaView>
 
                 {result.length > 0 ? (
                     <ScrollView
@@ -43,11 +78,11 @@ const SearchScreen = () => {
                                         onPress={() => navigation.push('Movie', item)}>
                                         <View className={`space-y-1 mb-4`}>
                                             <Image
-                                                source={require('../assets/images/moviePoster2.png')}
+                                                source={{uri: API_IMAGE_URL + item.poster_path}}
                                                 style={{ width: width * 0.44, height: height * 0.3 }}
                                                 className={`rounded-2xl`} />
                                             <Text className='text-white text-ellipsis'>
-                                                {movieName.length > 24 ? movieName.slice(0, 24) + '...' : movieName}
+                                                {item.title?.length > 24 ? item.title?.slice(0, 24) + '...' : item.title}
                                             </Text>
                                         </View>
                                     </TouchableWithoutFeedback>
@@ -59,7 +94,7 @@ const SearchScreen = () => {
                 ) : (
                     <View className='items-center'>
                         <Image source={require('../assets/images/movieTime.png')}
-                        className='h-96 w-96' />
+                            className='h-96 w-96' />
                     </View>
                 )}
 
